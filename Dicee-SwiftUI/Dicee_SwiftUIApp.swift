cb3ad82bf48fd81d33857c426611732ef198be4b
ponchotonic <ponchotonic@gmail.com>
2023-05-23 10:42:58 -0600

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Alfonso Castro Flores on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
